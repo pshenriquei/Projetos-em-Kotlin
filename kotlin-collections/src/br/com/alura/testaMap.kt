f0e4ced00a81d2bc4fe0a798657f630f52caaefe
Pedro Henrique Ignacio Sobrinho <82547663+pshenriquei@users.noreply.github.com>
2021-04-21 15:35:32 -0300

    Add files via upload
    Projeto voltado ao aprendizado de algumas collections em Kotlin.

---
+++ kotlin-collections/src/br/com/alura/testaMap.kt
@@ -0,0 +1,114 @@
+package br.com.alura
+
+fun testaComportamentosMap() {
+    val pedidos: MutableMap<Int, Double> = mutableMapOf(
+        Pair(1, 20.0),
+        Pair(2, 64.0),
+        3 to 50.0,
+        4 to 100.0,
+        5 to 100.0,
+        6 to 80.0
+
+    )
+//    val valorPedido = pedidos.getValue(5)
+//    println(valorPedido)
+    val message: Double? = pedidos.getOrElse(0, {
+        0.0
+    })
+    println(message)
+    println(pedidos.getOrDefault(1, -1.0))
+    println(pedidos.getOrDefault(0, -1.0))
+
+    println(pedidos.keys)
+    println(pedidos.values)
+
+    for (numero in pedidos.keys) {
+        println("Pedidos $numero")
+    }
+    println(pedidos.values)
+
+    for (valor in pedidos.values) {
+        println("Valor do pedido: $valor")
+    }
+
+    val pedidosFiltrados = pedidos.filter { (numero, valor) ->
+        numero % 2 == 0 && valor > 70.0
+    }
+    println(pedidosFiltrados)
+
+    val pedidosAcima = pedidos.filterValues { valor ->
+        valor > 70.0
+    }
+    println(pedidosAcima)
+
+    val pedidosPares = pedidos.filterKeys { numero ->
+        numero % 2 == 0
+    }
+    println(pedidosPares)
+
+    println(
+        pedidos + mapOf(
+            7 to 90.0,
+            8 to 20.0
+        )
+    )
+    println(pedidos)
+
+    println(pedidos - listOf(6, 5))
+    println(pedidos)
+
+//    pedidos.putAll(listOf<Pair<Int, Double>>(
+//        7 to 90.0,
+//        8 to 20.0,
+//        8 to 30.0
+//    ))
+    pedidos += listOf<Pair<Int, Double>>(
+        7 to 90.0,
+        8 to 20.0,
+        8 to 30.0
+    ) // mesma coisa de cima!
+    println(pedidos)
+
+    pedidos.keys.remove(1)
+    println(pedidos)
+
+    pedidos.values.remove(50.0)
+    println(pedidos)
+
+    pedidos.values.remove(100.0)
+    println(pedidos)
+
+    pedidos -= 6
+    println(pedidos)
+}
+
+fun testaMap(pedidos: MutableMap<Int, Double>) {
+    println(pedidos)
+    val pedido = pedidos[3]
+    pedido?.let {
+        println("pedido $it")
+    }
+
+    for (p: Map.Entry<Int, Double> in pedidos) {
+        println("Número do pedido: ${p.key}")
+        println("Valor do pedido: ${p.value}")
+    }
+
+    pedidos[4] = 70.0
+    println(pedidos)
+    pedidos.put(5, 80.0)
+    println(pedidos)
+    pedidos[1] = 100.0
+    println(pedidos)
+
+    pedidos.putIfAbsent(6, 200.0)
+    println(pedidos)
+
+    pedidos.putIfAbsent(6, 300.0)
+    println(pedidos)
+
+    pedidos.remove(6)
+    println(pedidos)
+    pedidos.remove(3, 50.0)
+    println(pedidos)
+}
---
+++ kotlin-collections/src/br/com/alura/testaSet.kt
@@ -0,0 +1,28 @@
+package br.com.alura
+
+fun testaSet() {
+    val assistiramCursoAndroid: Set<String> = setOf("Alex", "Fran", "Gui", "Maria")
+    val assistiramCursoKotlin: Set<String> = setOf("Alex", "Paulo", "Maria")
+//  val assistiramAmbos: Set<String> = assistiramCursoAndroid + assistiramCursoKotlin
+    val assistiramAmbos = mutableSetOf<String>()
+
+    assistiramAmbos.addAll(assistiramCursoAndroid)
+    assistiramAmbos.addAll(assistiramCursoKotlin)
+    assistiramAmbos.add("Pedro")
+    assistiramAmbos.add("Pedro")
+
+    println(assistiramAmbos)
+
+    println(assistiramCursoKotlin + assistiramCursoAndroid)
+    println(assistiramCursoKotlin union assistiramCursoAndroid)
+
+    println(assistiramCursoAndroid - assistiramCursoKotlin)
+    println(assistiramCursoAndroid subtract assistiramCursoKotlin)
+
+    println(assistiramCursoAndroid intersect assistiramCursoKotlin)
+
+    val assistiramList = assistiramAmbos.toMutableList()
+    assistiramList.add("Alex")
+    println(assistiramList)
+    println(assistiramList.toSet())
+}
